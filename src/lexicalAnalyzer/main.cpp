ad6ccf9c42b1fa97190af4b43d9ada561902f509
fsqier <kotovatana456@gmail.com>
2025-11-06 22:54:05 +0300

    Change main.cpp

--- src/lexicalAnalyzer/main.cpp
+++ src/lexicalAnalyzer/main.cpp
@@ -1,27 +1,18 @@
-#include "lexer.cxx"
+#include "lexer.h"
+#include "syntaxer.h"
 
 int main() {
+
     try {
-
-        std::ifstream in_program("Program.txt", std::ios::binary);
-        in_program.seekg(0, std::ios::end);
-        int size_text = in_program.tellg();
-        in_program.seekg(0, std::ios::beg);
-        char* text = new char[size_text];
-        in_program.read(text, size_text);
-
         Trie trie;
         std::ifstream in_key("KeyWords.txt", std::ios::binary);
         loadFromfile(in_key, trie);
-
-        const char* pos = text;
-        pair<Types, string> res = get_lexem(pos, text + size_text, trie);
-        while (res.first != Types::END) {
-            cout << res.second << " " << type_to_string(res.first) << '\n';
-            res = get_lexem(pos, text + size_text, trie);
-        }
-    }
-    catch (...) {
-        std::cerr << "Trie error: No caps in programm";
+        Lexer lexer("Program.txt", trie);
+        Syntaxer syntaxer(lexer);
+        syntaxer.syntax();
+    } catch (string s) {
+        std::cerr << s;
+    } catch (Types t) {
+        std::cerr << type_to_string(t);
     }
 }
