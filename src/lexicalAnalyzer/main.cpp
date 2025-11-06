710d506a8a99da9073683ae8a6ebaf7f0a6e271a
fsqier <kotovatana456@gmail.com>
2025-11-07 00:50:33 +0300

    Change main.cpp

--- src/lexicalAnalyzer/main.cpp
+++ src/lexicalAnalyzer/main.cpp
@@ -10,9 +10,15 @@ int main() {
         Lexer lexer("Program.txt", trie);
         Syntaxer syntaxer(lexer);
         syntaxer.syntax();
-    } catch (string s) {
-        std::cerr << s;
-    } catch (Types t) {
-        std::cerr << type_to_string(t);
+    } catch (pair<Lexem, pair<Types, string>> error) {
+        std::cerr << type_to_string(error.first.type) << " " << error.first.value << '\n'
+            << "in line:" << error.first.line << " column:" << error.first.column << '\n'
+            << "need " << type_to_string(error.second.first) << " " << error.second.second;
+    } catch (pair<Lexem, Types> error) {
+        std::cerr << type_to_string(error.first.type) << " " << error.first.value << '\n'
+            << "in line:" << error.first.line << " column:" << error.first.column << '\n'
+            << "need " << type_to_string(error.second);
+    } catch (Types type) {
+        std::cerr << "where type"; //я не придумала как тут лучше кидать исключения... пока так
     }
 }
